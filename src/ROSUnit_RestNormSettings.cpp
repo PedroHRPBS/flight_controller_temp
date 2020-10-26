78d5ee0a9ac4ea035bc3b71944612a584fe91afb
PedroHRPBS <pedrohenriquerpbs@gmail.com>
2020-10-26 15:51:52 +0400

    ROSUnit_RestNormSettings done.

--- src/ROSUnit_RestNormSettings.cpp
+++ src/ROSUnit_RestNormSettings.cpp
@@ -6,14 +6,27 @@ RestrictedNormRefSettingsMsg ROSUnit_RestNormSettings::_settings_msg;
 ROSUnit_RestNormSettings::ROSUnit_RestNormSettings(ros::NodeHandle& t_main_handler) : ROSUnit(t_main_handler) {
     _srv_rest_norm_settings = t_main_handler.advertiseService("restricted_norm_settings", callbackSettings);    
     _instance_ptr = this;
+    this->_output_port = new OutputPort(ports_id::OP_0_DATA, this);
+    _ports = {_output_port};
 }   
 
 ROSUnit_RestNormSettings::~ROSUnit_RestNormSettings() {
 
 }
 
-void ROSUnit_RestNormSettings::receiveMsgData(DataMessage* t_msg){
+void ROSUnit_RestNormSettings::process(DataMessage* t_msg, Port* t_port){
 
+}
+
+std::vector<Port*> ROSUnit_RestNormSettings::getPorts(){
+    return this->_ports;
+}
+
+DataMessage* ROSUnit_RestNormSettings::runTask(DataMessage* t_msg){
+    this->_output_port->receiveMsgData(t_msg);
+}
+
+void ROSUnit_RestNormSettings::receiveMsgData(DataMessage* t_msg){
 
 }
 
@@ -24,7 +37,7 @@ bool ROSUnit_RestNormSettings::callbackSettings(flight_controller::Restricted_No
     _settings_msg.delete_existing_waypoints = req.delete_existing_waypoints;
     _settings_msg.setMaxNorm(req.max_norm);
 
-    _instance_ptr->emitMsgUnicastDefault((DataMessage*) &_settings_msg);
+    _instance_ptr->runTask((DataMessage*) &_settings_msg);
 
     return true;
 
