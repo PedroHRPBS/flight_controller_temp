8bd70ecf53636fd869724dcd2c1e025a8df87024
PedroHRPBS <pedrohenriquerpbs@gmail.com>
2020-10-27 07:16:48 +0400

    Saturation fix.

--- src/Saturation.cpp
+++ src/Saturation.cpp
@@ -11,48 +11,22 @@ Saturation::~Saturation() {
 
 }
 
-// void Saturation::receiveMsgData(DataMessage* t_msg, int t_channel){
-
-//     if(t_msg->getType() == msg_type::FLOAT){
-
-//         FloatMsg* float_msg = (FloatMsg*)t_msg;
-//         FloatMsg output;
-//         output.data = float_msg->data;
-
-//         if(output.data > _clip_value){
-//             output.data = _clip_value;
-//         }else if(output.data < -_clip_value){
-//             output.data = -_clip_value;
-//         }
-
-//         this->emitMsgUnicast((DataMessage*) &output,
-//                                     -1,
-//                                     t_channel);   
-//     }
-// }
-
 DataMessage* Saturation::runTask(DataMessage* t_msg){
-    
-    FloatMsg* float_msg = new FloatMsg();
-    float_msg->data = _input;
-
-    if(float_msg->data > _clip_value){
-        float_msg->data = _clip_value;
-    }
-    else if(float_msg->data < -_clip_value){
-        float_msg->data = -_clip_value;
-    }
-    
-    this->_output_port->receiveMsgData(float_msg);
-
     return t_msg; //TODO no need for t_msg
 }
 
 void Saturation::process(DataMessage* t_msg, Port* t_port) {
     if(t_port->getID() == ports_id::IP_0_DATA){
         FloatMsg* float_msg = (FloatMsg*)t_msg;
-        float data = float_msg->data;
-        _input = data;
+
+        if(float_msg->data > _clip_value){
+            float_msg->data = _clip_value;
+        }
+        else if(float_msg->data < -_clip_value){
+            float_msg->data = -_clip_value;
+        }
+        
+        this->_output_port->receiveMsgData(float_msg);
     }
 }
 
